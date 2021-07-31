0a970134aed437045eb4a64b31456e5874f03026
pforderique <porderiq@mit.edu>
2021-07-31 18:20:47 -0400

    Version 1.1 complete

--- sketch.js
+++ sketch.js
@@ -51,7 +51,7 @@ let timer;
 function setup() {
   createCanvas(BOARD_WIDTH, BOARD_HEIGHT + 2*EDGE_HEIGHT);
   currentPlayer = int(random(0, 2)); // select random player
-  console.log("WELCOME TO TIC TAC TOE w/Minimax");
+  // console.log("WELCOME TO TIC TAC TOE w/Minimax");
 
   LIGHT_YELLOW = color('#FEFEE3');
   BLUE_GRAY = color('#1A5E63');
@@ -281,7 +281,7 @@ function calculateBestMove() {
     }
   }
   grid[bestMove[0]][bestMove[1]] = 'O';
-  console.log("Computer placed move");
+  // console.log("Computer placed move");
 }
 
 function minimax(depth, maximizing) {
@@ -347,7 +347,6 @@ function gameWon(recordWinningCoors){  //* Returns {null, 'tie', 'human', or 'co
     if (xsum == 3 || osum == 3) {
       if (recordWinningCoors) { 
         winningLineCoors.push(row, 0, row, grid[0].length - 1); 
-        console.log("game won. winning line: " + winningLineCoors);
       }
       if(xsum == 3){return "human"}
       if(osum == 3){return "computer"}
@@ -365,7 +364,6 @@ function gameWon(recordWinningCoors){  //* Returns {null, 'tie', 'human', or 'co
     if (xsum == 3 || osum == 3) {
       if (recordWinningCoors) { 
         winningLineCoors.push(0, col, grid.length - 1, col); 
-        console.log("game won. winning line: " + winningLineCoors);
       }
       if(xsum == 3){return "human"}
       if(osum == 3){return "computer"}
@@ -386,7 +384,6 @@ function gameWon(recordWinningCoors){  //* Returns {null, 'tie', 'human', or 'co
   if (x1sum == 3 || o1sum == 3) {
     if (recordWinningCoors) { 
       winningLineCoors.push(0, 0, grid.length - 1, grid[0].length - 1); 
-      console.log("game won. winning line: " + winningLineCoors);
     }
     if(x1sum == 3){return "human"}
     if(o1sum == 3){return "computer"}
@@ -394,7 +391,6 @@ function gameWon(recordWinningCoors){  //* Returns {null, 'tie', 'human', or 'co
   if (x2sum == 3 || o2sum == 3) {
     if (recordWinningCoors) { 
       winningLineCoors.push(grid.length - 1, 0, 0, grid[0].length - 1); 
-      console.log("game won. winning line: " + winningLineCoors);
     }
     if(x2sum == 3){return "human"}
     if(o2sum == 3){return "computer"}
@@ -454,7 +450,7 @@ function mouseClicked(){
 
       // place player's symbol there IF empty and update current player
       if(grid[row][col] !== ''){
-        console.log("INVALID MOVE");
+        // console.log("INVALID MOVE");
         break;
       } else {
         grid[row][col] = 'X';
